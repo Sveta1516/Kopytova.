f84482152b2b509d23c10aceb42aef3adbb98626
Sveta1516 <165479139+Sveta1516@users.noreply.github.com>
2026-09-19 16:49:16 +0500

    Add files via upload

---
+++ _Kopytova/_Kopytova/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _Kopytova
+{
+    public class Class1
+    {
+
+        public string i_Nomer { get; set; }
+        public string naz { get; set; }
+        public string color { get; set; }
+        public string ispol { get; set; }
+
+        public virtual void WarningSound()
+        {
+            Console.WriteLine("Устройство подаёт сигнал");
+        }
+    }
+
+    public class Thermometer : Class1
+    {
+        private double temperatura { get; set; }
+
+        public void TempSearch()
+        {
+            temperatura = new Random().Next(36, 40);
+            Console.WriteLine($"Температура = {temperatura}");
+        }
+        public override void WarningSound()
+        {
+            Console.WriteLine("Мигает");
+        }
+    }
+    public class Weight : Class1
+    {
+        private double ves { get; set; }
+
+        public void WeightSearch()
+        {
+            ves = new Random().Next(50, 75);
+            Console.WriteLine($"Вес = {ves}");
+        }
+
+        public override void WarningSound()
+        {
+            Console.WriteLine("Издает звук");
+        }
+    }
+
+    public class Barometr : Class1
+    {
+        private double davlenie { get; set; }
+
+        public void BarSearch()
+        {
+            davlenie = new Random().Next(50, 75);
+            Console.WriteLine($"Давление = {davlenie}");
+        }
+        public override void WarningSound()
+        {
+            Console.WriteLine("Свистит");
+        }
+    }
+}
---
+++ _Kopytova/_Kopytova/Program.cs
@@ -0,0 +1,27 @@
+﻿using _Kopytova;
+
+Class1 pribor = new Class1();
+Thermometer termometr = new Thermometer();
+Weight vesy = new Weight();
+Barometr barometr = new Barometr();
+
+Console.WriteLine("Введите Инв.Номер");
+pribor.i_Nomer = Console.ReadLine();
+
+Console.WriteLine("Введите название");
+pribor.naz = Console.ReadLine();
+
+Console.WriteLine("Введите цвет");
+pribor.color = Console.ReadLine();
+
+Console.WriteLine("Введите исполнение");
+pribor.ispol= Console.ReadLine();
+
+termometr.WarningSound();
+termometr.TempSearch();
+
+vesy.WarningSound();
+vesy.WeightSearch();
+
+barometr.WarningSound();
+barometr.BarSearch();
